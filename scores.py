3118843a44c3c1f891b52cbb2f377bce83e91bb8
Audrey <ale314@uw.edu>
2024-11-15 16:34:51 -0600

    more gsheets magic

---
+++ scores.py
@@ -0,0 +1,33 @@
+import sqlite3
+
+import gspread
+
+gc = gspread.service_account(filename='/Users/mermaid/PycharmProjects/AccessMap/accessmap-441715-705d96e6c09e.json')
+
+sh = gc.open_by_key("1O01Exonl72cF3G5ZYZzYgYJ55ObHaqrp0dXCHeZ4L-E")
+
+
+
+# print(sh.sheet1.get('A2:A1000'))
+
+# worksheet = sh.worksheet("Sheet2")
+#
+# cell = worksheet.find("Chicago Yacht Club")
+# print("Found something at R%sC%s" % (cell.row, cell.col))
+#
+# lookup_function = worksheet.findall('11', in_column=1)
+# print(lookup_function)
+#
+# lookup_name = worksheet.find("Chicago Yacht Club", in_column=2)
+
+def query_sensory_rating_sql():
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+    sql = ('select Name, AVG(SensoryRating) from ratings group by 1')
+    cursor.execute(sql)
+    return cursor.fetchall()
+
+result = query_sensory_rating_sql()
+
+# for tuple in result:
+#     print(tuple[0])
