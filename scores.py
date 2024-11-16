d867da06502defcd5f29a1ca31aab24909cfa499
audreydottech <audreydottech@gmail.com>
2024-11-16 00:31:58 -0600

    Update scores.py

--- scores.py
+++ scores.py
@@ -2,23 +2,10 @@ import sqlite3
 
 import gspread
 
-gc = gspread.service_account(filename='/Users/mermaid/PycharmProjects/AccessMap/accessmap-441715-705d96e6c09e.json')
+gc = gspread.service_account(filename='accessmap-441715-705d96e6c09e.json')
 
 sh = gc.open_by_key("1O01Exonl72cF3G5ZYZzYgYJ55ObHaqrp0dXCHeZ4L-E")
 
-
-
-# print(sh.sheet1.get('A2:A1000'))
-
-# worksheet = sh.worksheet("Sheet2")
-#
-# cell = worksheet.find("Chicago Yacht Club")
-# print("Found something at R%sC%s" % (cell.row, cell.col))
-#
-# lookup_function = worksheet.findall('11', in_column=1)
-# print(lookup_function)
-#
-# lookup_name = worksheet.find("Chicago Yacht Club", in_column=2)
 
 def query_sensory_rating_sql():
     conn = sqlite3.connect('database.db')
@@ -27,7 +14,36 @@ def query_sensory_rating_sql():
     cursor.execute(sql)
     return cursor.fetchall()
 
-result = query_sensory_rating_sql()
 
-# for tuple in result:
-#     print(tuple[0])
+def query_mobility_rating_sql():
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+    sql = ('select Name, AVG(MobilityRating) from ratings group by 1')
+    cursor.execute(sql)
+    return cursor.fetchall()
+
+
+def query_service_dog_relief_rating_sql():
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+    sql = ('select Name, AVG(ServiceDogRelief) from ratings group by 1')
+    cursor.execute(sql)
+    return cursor.fetchall()
+
+
+def query_wheelchair_accessible_rating_sql():
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+    sql = ('select Name, AVG(WheelchairAccessible) from ratings group by 1')
+    cursor.execute(sql)
+    return cursor.fetchall()
+
+
+def query_allergen_risk_rating_sql():
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+    sql = ('select Name, AVG(CommonAllergenRisk) from ratings group by 1')
+    cursor.execute(sql)
+    return cursor.fetchall()
+
+
