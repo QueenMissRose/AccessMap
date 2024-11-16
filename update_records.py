1c586b38de21ae2fda816dae63956c4bd03d9c85
jglancy95 <jasmine.glancy.vt@gmail.com>
2024-11-16 10:37:59 -0500

    update punctuation

--- update_records.py
+++ update_records.py
@@ -6,7 +6,7 @@ gc = gspread.service_account(filename='accessmap-441715-705d96e6c09e.json')
 
 sh = gc.open_by_key("1O01Exonl72cF3G5ZYZzYgYJ55ObHaqrp0dXCHeZ4L-E")
 
-# TODO: Fix update location rating
+# TODO: Fix update location rating by ID
 def update_location_rating(location_id):
     conn = sqlite3.connect('database.db')
     cursor = conn.cursor()
